9b0cd423b2b0e699d8f51aa816c853b17c213fed
jpham197 <jpham197@gmail.com>
2017-03-21 10:54:30 -0400

    Added Fortune class to be used for later

---
+++ Fortune.java
@@ -0,0 +1,13 @@
+//to be used later to separate fortunes into their own class rather than store them as arrays
+
+public class Fortune {
+	private String fortune;
+
+	public Fortune(String fortune) {
+		this.fortune = fortune;
+	}
+
+	public string toString() {
+		return fortune;
+	}
+}
